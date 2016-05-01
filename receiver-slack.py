0b59d585c8c654528ff1d09e3d9a4c002a2b6fda
EC2 Default User <ec2-user@ip-172-31-31-224.ec2.internal>
2016-05-01 22:46:19 +0000

    Extended slack functionality on the receiver

--- receiver-slack.py
+++ receiver-slack.py
@@ -4,6 +4,7 @@ import urllib
 import time
 import pprint
 from slackclient import SlackClient
+import json
 
 #Create a config file for these variables later
 hostname = ""
@@ -12,31 +13,43 @@ pp = pprint.PrettyPrinter(indent=4)
 
 class HookReceiver(BaseHTTPRequestHandler):
 		def do_GET(self):
-			self.send_response(400)
-			self.send_header("Content-type", "text/json")
-			self.end_headers()
-			return
+                    self.send_response(400)
+                    self.send_header("Content-type", "text/json")
+                    self.end_headers()
+                    return
 
 		def _deliver(self, contents):
-			token = "test123"
-			sc = SlackClient(token)
-			return sc.api_call("chat.postMessage", channel="#general", text=contents, username="webdog-bot")
-
+                    token = "test123"
+                    sc = SlackClient(token)
+                    return sc.api_call("chat.postMessage", channel="#general", text=contents, username="webdog-bot")
 
 		def do_POST(self):
-			length = int(self.headers['Content-Length'])
-			post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
-			self.send_response(200)
-			self.send_header("Content-type", "text/json")
-			self.end_headers()
-			return post_data
+                    length = int(self.headers['Content-Length'])
+                    #post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
+                    action = self.headers['X-GitHub-Event']
+                    post_data = json.loads(self.rfile.read(length).decode("utf-8"))
+                    if "create" in action:
+                        contents = (action, post_data['ref'])
+                        self._deliver(contents)
+                    elif "pull_request" in action:
+                        print("found a pull request event")
+                        url = post_data['pull_request']['url']
+                        title = post_data['pull_request']['title']
+                        contents = "Pull Request %s(%s) was %s\n%s" %(post_data['number'], title, post_data['action'], url)
+                        print(contents)
+                        self._deliver("Beep-Boop Hubot alert")
+                        self._deliver(contents)
+
+                    self.send_response(200)
+                    self.send_header("Content-type", "text/json")
+                    self.end_headers()
 
 if __name__ == '__main__':
-	httpd = HTTPServer((hostname, hostport), HookReceiver)
-	print(time.asctime(), "Server Starts - %s:%s" % (hostname, hostport))
-	try:
-		httpd.serve_forever()
-	except KeyboardInterrupt:
-		pass
-	httpd.server_close()
-	print(time.asctime(), "Server Stops - %s:%s" % (hostname, hostport))
+    httpd = HTTPServer((hostname, hostport), HookReceiver)
+    print(time.asctime(), "Server Starts - %s:%s" % (hostname, hostport))
+    try:
+            httpd.serve_forever()
+    except KeyboardInterrupt:
+            pass
+    httpd.server_close()
+    print(time.asctime(), "Server Stops - %s:%s" % (hostname, hostport))
